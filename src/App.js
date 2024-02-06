13ae8ba4d304aebba3f326b3b3dd43443603585d
AdityaMishra3648 <123090809+AdityaMishra3648@users.noreply.github.com>
2024-02-06 13:05:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState("white")
+  let changeColor = (col) =>{
+    setColor(col);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen p-0 m-0" style={{backgroundColor : color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+
+        <button className='bg-green-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("green")}>Green</button>
+        <button className='bg-pink-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("pink")}>Pink</button>
+        <button className='bg-purple-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("purple")}>Purple</button>
+        <button className='bg-gray-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("gray")}>Gray</button>
+        <button className='bg-red-500 outline-none rounded-full  px-4 py-1' onClick={()=>changeColor("red")}>Red</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
